acbd923fadcb4bc1a6fd26c9452f275199140303
Uttam Kumar <uttamkumar050519@gmail.com>
2024-06-18 23:09:12 +0530

    chai aur django

--- testTheChai/testTheChai/urls.py
+++ testTheChai/testTheChai/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangoTest project.
+URL configuration for testTheChai project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
